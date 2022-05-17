b9be2cad938d28189d53f352b8e926094e07e1e2
Luiz Henrique <luizhenrique.ecl@gmail.com>
2022-05-17 12:03:48 -0300

    Creates user repository

---
+++ src/main/java/com/example/voting/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.voting.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.voting.model.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	Iterable<User> findByNameContaining(@Param("name") String name);
+}
