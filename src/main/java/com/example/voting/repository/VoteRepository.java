41e79affa6c8deb89ce3ab6426d430529a3f4452
Luiz Henrique <luizhenrique.ecl@gmail.com>
2022-05-19 20:24:33 -0300

    Check vote already exists

--- src/main/java/com/example/voting/repository/VoteRepository.java
+++ src/main/java/com/example/voting/repository/VoteRepository.java
@@ -2,8 +2,11 @@ package com.example.voting.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.voting.model.Session;
+import com.example.voting.model.User;
 import com.example.voting.model.Vote;
 
 public interface VoteRepository extends CrudRepository<Vote, Integer> {
 
+	boolean existsByUserAndSession(User user, Session session);
 }
--- src/main/java/com/example/voting/service/VoteService.java
+++ src/main/java/com/example/voting/service/VoteService.java
@@ -1,6 +1,7 @@
 package com.example.voting.service;
 
 import com.example.voting.dto.vote.VoteSessionDTO;
+import com.example.voting.exception.VoteAlreadyExistsException;
 import com.example.voting.model.Vote;
 import com.example.voting.repository.VoteRepository;
 import com.example.voting.transformer.vote.RegisterVoteTransformer;
@@ -8,8 +9,10 @@ import com.example.voting.transformer.vote.RegisterVoteTransformer;
 @org.springframework.stereotype.Service
 public class VoteService extends Service<Vote, VoteRepository> {
 
-	public int registerVote(VoteSessionDTO voteDTO) {
+	public int registerVote(VoteSessionDTO voteDTO) throws VoteAlreadyExistsException {
 		Vote vote = RegisterVoteTransformer.transform(voteDTO);
+		if (repository.existsByUserAndSession(vote.getUser(), vote.getSession()))
+			throw new VoteAlreadyExistsException("vote alredy exist");
 		return save(vote);
 	}
 }
