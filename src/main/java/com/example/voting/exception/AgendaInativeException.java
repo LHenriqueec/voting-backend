a81e56619f50eefedc8cb1268b44eee36360dc4f
Luiz Henrique <luizhenrique.ecl@gmail.com>
2022-05-19 21:36:53 -0300

    Creates exception to inactive agenda

---
+++ src/main/java/com/example/voting/exception/AgendaInativeException.java
@@ -0,0 +1,9 @@
+package com.example.voting.exception;
+
+public class AgendaInativeException extends RuntimeException {
+	private static final long serialVersionUID = -2279998096715384453L;
+
+	public AgendaInativeException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/example/voting/service/VoteService.java
+++ src/main/java/com/example/voting/service/VoteService.java
@@ -1,7 +1,9 @@
 package com.example.voting.service;
 
 import com.example.voting.dto.vote.VoteSessionDTO;
+import com.example.voting.exception.AgendaInativeException;
 import com.example.voting.exception.VoteAlreadyExistsException;
+import com.example.voting.model.Session;
 import com.example.voting.model.Vote;
 import com.example.voting.repository.VoteRepository;
 import com.example.voting.transformer.vote.RegisterVoteTransformer;
@@ -11,8 +13,14 @@ public class VoteService extends Service<Vote, VoteRepository> {
 
 	public int registerVote(VoteSessionDTO voteDTO) throws VoteAlreadyExistsException {
 		Vote vote = RegisterVoteTransformer.transform(voteDTO);
-		if (repository.existsByUserAndSession(vote.getUser(), vote.getSession()))
+		Session session = vote.getSession();
+
+		if (repository.existsByUserAndSession(vote.getUser(), session))
 			throw new VoteAlreadyExistsException("vote alredy exist");
-		return save(vote);
+
+		if (session.getAgenda().isActive())
+			return save(vote);
+		else
+			throw new AgendaInativeException("agenda inative");
 	}
 }
