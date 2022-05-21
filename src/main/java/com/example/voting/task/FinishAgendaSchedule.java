66440768e14619b47eddfead162b09f8a5c4272e
Luiz Henrique <luizhenrique.ecl@gmail.com>
2022-05-21 11:48:36 -0300

    Creates a finish agenda schedule

---
+++ src/main/java/com/example/voting/task/FinishAgendaSchedule.java
@@ -0,0 +1,19 @@
+package com.example.voting.task;
+
+import com.example.voting.service.AgendaService;
+
+public class FinishAgendaSchedule implements Runnable {
+	private final int id;
+	private AgendaService service;
+
+	public FinishAgendaSchedule(int id) {
+		this.id = id;
+		this.service = new AgendaService();
+	}
+
+	@Override
+	public void run() {
+		service.finishAgenda(id);
+	}
+
+}
